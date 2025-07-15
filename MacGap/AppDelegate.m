5410111f25c15f4214006885c8458b9dc47d4040
Jonathan Alland <jonathan@Mavericks-VM.local>
2025-07-15 17:46:39 -0400

    checkpoint

--- MacGap/AppDelegate.m
+++ MacGap/AppDelegate.m
@@ -1,11 +1,3 @@
-//
-//  AppDelegate.m
-//  MacGap
-//
-//  Created by Alex MacCaw on 08/01/2012.
-//  Copyright (c) 2012 Twitter. All rights reserved.
-//
-
 #import "AppDelegate.h"
 
 @implementation AppDelegate
@@ -14,7 +6,6 @@
 
 - (void) applicationWillFinishLaunching:(NSNotification *)aNotification
 {
-
     
 }
 
@@ -26,12 +17,52 @@
     return YES;
 }
 
-- (void) applicationDidFinishLaunching:(NSNotification *)aNotification { 
+- (void) applicationDidFinishLaunching:(NSNotification *)aNotification {
     self.windowController = [[WindowController alloc] initWithURL: kStartPage];
     [self.windowController showWindow: [NSApplication sharedApplication].delegate];
     self.windowController.contentView.webView.alphaValue = 1.0;
     self.windowController.contentView.alphaValue = 1.0;
     [self.windowController showWindow:self];
+    
+    // Apply window constraints from Info.plist
+    [self applyWindowConstraints];
+}
+
+- (void)applyWindowConstraints {
+    // Get values from Info.plist
+    NSString *minWidthString = [[NSBundle mainBundle] objectForInfoDictionaryKey:@"MinimumWindowWidth"];
+    NSString *minHeightString = [[NSBundle mainBundle] objectForInfoDictionaryKey:@"MinimumWindowHeight"];
+    NSString *maxWidthString = [[NSBundle mainBundle] objectForInfoDictionaryKey:@"MaximumWindowWidth"];
+    NSString *maxHeightString = [[NSBundle mainBundle] objectForInfoDictionaryKey:@"MaximumWindowHeight"];
+    
+    // Convert strings to CGFloat values with defaults
+    CGFloat minWidth = minWidthString ? [minWidthString doubleValue] : 400.0;
+    CGFloat minHeight = minHeightString ? [minHeightString doubleValue] : 300.0;
+    CGFloat maxWidth = maxWidthString ? [maxWidthString doubleValue] : CGFLOAT_MAX;
+    CGFloat maxHeight = maxHeightString ? [maxHeightString doubleValue] : CGFLOAT_MAX;
+    
+    // Set minimum window size
+    self.windowController.window.minSize = NSMakeSize(minWidth, minHeight);
+    
+    // Set maximum window size
+    self.windowController.window.maxSize = NSMakeSize(maxWidth, maxHeight);
+    
+    // Disable full screen if maximum width or height is specified
+    if (maxWidthString || maxHeightString) {
+        NSWindowCollectionBehavior behavior = self.windowController.window.collectionBehavior;
+        behavior &= ~NSWindowCollectionBehaviorFullScreenPrimary;
+        self.windowController.window.collectionBehavior = behavior;
+    }
+}
+
+- (BOOL)applicationShouldTerminateAfterLastWindowClosed:(NSApplication *)theApplication {
+    id terminateAfterLastWindowClosed = [[NSBundle mainBundle] objectForInfoDictionaryKey:@"TerminateAfterLastWindowClosed"];
+    
+    if (terminateAfterLastWindowClosed && [terminateAfterLastWindowClosed isKindOfClass:[NSNumber class]]) {
+        return [terminateAfterLastWindowClosed boolValue];
+    }
+    
+    return NO;
 }
 
 @end
